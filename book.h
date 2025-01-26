79eb0850d1508e7cbbb8c1cb42a7bd4449701975
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2025-01-26 17:20:20 +0530

    Create book.h

---
+++ book.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+
+class Book
+{
+private:
+    string name;
+    int rating;
+    string type; 
+
+public:
+    void set_name(string name);
+    string get_name();
+
+    void set_rating(int rating);
+    int get_rating();
+
+    void set_type(string type);  
+    string get_type(); 
+
+    virtual void displayDetails() = 0;
+
+};
+
+class fiction_book : public Book 
+{
+private:
+    string genre;
+
+public:
+    void set_genre(string genre);
+    string get_genre();
+
+    void displayDetails(); 
+};
+
+
+class non_fiction_book : public Book 
+{
+private:
+    string subject;
+
+public:
+    void set_subject(string subject);
+    string get_subject();
+
+    void displayDetails();
+};
